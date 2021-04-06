5b9ce94f45e640a056fa65bafa0d5492484fad16
Graham Seamans <seamansgraham@gmail.com>
2021-04-06 10:35:10 -0700

    renamed train_test to process, made ephocs a class variable

--- perceptrons.py
+++ perceptrons.py
@@ -15,6 +15,7 @@ class perceptron_learning:
         self.prep_input_data()
 
         self.lrate = 0.001
+        self.epochs = 5
         self.weights = np.random.rand(785, 10) - 0.5
 
     def train_data(self):
@@ -24,27 +25,16 @@ class perceptron_learning:
         return zip(self.test_image_data, self.test_label_data)
 
     def run(self):
-        epochs = 30
         train_correct = []
         test_correct = []
-        train_correct.append(self.train_test(self.train_data(), learn=False))
-        test_correct.append(self.train_test(self.test_data(), learn=False))
-        for _ in range(epochs):
-            train_correct.append(self.train_test(self.train_data(), learn=True))
-            test_correct.append(self.train_test(self.test_data(), learn=False))
-        self.plot_epoch_pred_rate(epochs, test_correct, train_correct)
+        train_correct.append(self.process(self.train_data(), learn=False))
+        test_correct.append(self.process(self.test_data(), learn=False))
+        for _ in range(self.epochs):
+            train_correct.append(self.process(self.train_data(), learn=True))
+            test_correct.append(self.process(self.test_data(), learn=False))
+        self.plot_epoch_pred_rate(test_correct, train_correct)
 
-    def plot_epoch_pred_rate(self, epochs, test_correct, train_correct):
-        epochs_axis = list(range(epochs + 1))
-        plt.plot(epochs_axis, test_correct, label="test")
-        plt.plot(epochs_axis, train_correct, label="train")
-        plt.title("learning")
-        plt.xlabel("epochs")
-        plt.ylabel("correct pred. rate")
-        plt.legend()
-        plt.show()
-
-    def train_test(self, data, learn):
+    def process(self, data, learn):
         guesses = 0
         wrong_guesses = 0
         for in_vect, label in data:
@@ -80,6 +70,16 @@ class perceptron_learning:
         data = np.insert(data, 28 ** 2, 1, axis=1)
         return data
 
+    def plot_epoch_pred_rate(self, test_correct, train_correct):
+        epochs_axis = list(range(self.epochs + 1))
+        plt.plot(epochs_axis, test_correct, label="Test")
+        plt.plot(epochs_axis, train_correct, label="Train")
+        plt.title("Learning")
+        plt.xlabel("Epochs")
+        plt.ylabel("Correct pred. rate")
+        plt.legend()
+        plt.show()
+
     def read_mnist_file(self, path):
         with open(path, "rb") as f:
             _, _, d_type, num_dimensions = np.fromfile(f, dtype=np.dtype(">B"), count=4)
@@ -90,12 +90,5 @@ class perceptron_learning:
 
 
 if __name__ == "__main__":
-    profiler = cProfile.Profile()
-    profiler.enable()
-
     perceptrons = perceptron_learning()
     perceptrons.run()
-
-    profiler.disable()
-    stats = pstats.Stats(profiler)
-    stats.strip_dirs().sort_stats("tottime").print_stats(10)
