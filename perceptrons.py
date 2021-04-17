03af25be7d7dac2781cc6350f0aaa1c090d4e469
Graham Seamans <seamansgraham@gmail.com>
2021-04-16 20:01:43 -0700

    added confusion matrix, max_epochs

--- perceptrons.py
+++ perceptrons.py
@@ -14,9 +14,9 @@ class perceptron_learning:
         self.test_label_data = self.read_mnist_file("train-labels-idx1-ubyte")
         self.prep_input_data()
 
-        self.lrate = 0.001
-        self.epochs = 5
-        self.weights = np.random.rand(785, 10) - 0.5
+        self.lrate = 0.1
+        self.max_epochs = 5
+        self.weights = (np.random.rand(785, 10) - 0.5) / 10
 
     def train_data(self):
         return zip(self.train_image_data, self.train_label_data)
@@ -25,14 +25,19 @@ class perceptron_learning:
         return zip(self.test_image_data, self.test_label_data)
 
     def run(self):
-        train_correct = []
-        test_correct = []
+        epoch = 0
+        train_correct = [0]
+        test_correct = [0]
         train_correct.append(self.process(self.train_data(), learn=False))
         test_correct.append(self.process(self.test_data(), learn=False))
-        for _ in range(self.epochs):
+        while train_correct[-1] - train_correct[-2] > 0.01 and epoch < self.max_epochs:
+            epoch += 1
             train_correct.append(self.process(self.train_data(), learn=True))
             test_correct.append(self.process(self.test_data(), learn=False))
+        train_correct.pop(0)
+        test_correct.pop(0)
         self.plot_epoch_pred_rate(test_correct, train_correct)
+        self.get_confusion_matrix(self.test_data())
 
     def process(self, data, learn):
         guesses = 0
@@ -45,6 +50,17 @@ class perceptron_learning:
                 if learn:
                     self.learn(in_vect, label, out)
         return 1 - wrong_guesses / guesses
+
+    def get_confusion_matrix(self, data):
+        conf_matrix = np.zeros((10, 10))
+        for in_vect, label in data:
+            out = in_vect @ self.weights
+            guess = np.argmax(out)
+            conf_matrix[label][guess] += 1
+        conf_matrix = conf_matrix / np.linalg.norm(conf_matrix)
+        fig, ax = plt.subplots()
+        im = ax.imshow(conf_matrix)
+        fig.tight_layout()
 
     def learn(self, in_vect, label, out):
         error = self.one_hot_encoder(label) - self.activate(out)
@@ -61,14 +77,13 @@ class perceptron_learning:
         return out
 
     def plot_epoch_pred_rate(self, test_correct, train_correct):
-        epochs_axis = list(range(self.epochs + 1))
-        plt.plot(epochs_axis, test_correct, label="Test")
+        epochs_axis = list(range(len(train_correct)))
         plt.plot(epochs_axis, train_correct, label="Train")
+        plt.plot(epochs_axis, test_correct, label="Test")
         plt.title("Learning")
         plt.xlabel("Epochs")
         plt.ylabel("Correct pred. rate")
         plt.legend()
-        plt.show()
 
     def read_mnist_file(self, path):
         with open(path, "rb") as f:
@@ -92,3 +107,4 @@ class perceptron_learning:
 if __name__ == "__main__":
     perceptrons = perceptron_learning()
     perceptrons.run()
+    plt.show()
